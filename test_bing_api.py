f242fa62df99f8dc58e7e4c699ae2f9433c12346
Jia Shu <68574446+jiashu-sh@users.noreply.github.com>
2023-06-20 13:30:10 +0800

    测试搜索增加输入交互

--- test_bing_api.py
+++ test_bing_api.py
@@ -1,9 +1,25 @@
-
+import time
 from bing_api import BingApi
 
 if __name__ == "__main__" :
-    sContent = "LLM大模型"
+    sContentDefault = "LLM大模型"
+    
+    sTxtGet = input("请输入生成内容:\n")
+    if len(sTxtGet.strip()) == 0:
+        sTxtGet = sContentDefault
+    sConfirm = input("您输入内容为:\n  {}\n是否确认?(y/n)".format(sTxtGet))
+    
+    if (sConfirm.strip().lower()) == "n":
+        print("用户退出")
+        exit(0) # 执行完成自由对话模式，退出。
+    
+    
+    # 执行标准全文一键生成模式（耗时较长）
+    print("执行中，请稍候...")
+    start_time = time.time()
+    
     bing = BingApi()
-    contacts = bing.Search(search_text=sContent,s_count=5)
+    contacts = bing.Search(search_text=sTxtGet,s_count=5)
+    print("查询耗时： {} (s)".format(str(time.time()-start_time)))
     print("result count: {}".format(len(contacts)))
     print(contacts)
